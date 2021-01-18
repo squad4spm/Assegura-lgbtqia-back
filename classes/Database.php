06fd4bceacb8a3400a902f9341f65ed068af331a
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-18 10:50:17 -0300

    Att charfset uft8

--- classes/Database.php
+++ classes/Database.php
@@ -6,11 +6,13 @@ class Database{
     private $db_name = 'assegura';
     private $db_username = 'root';
     private $db_password = '';
+    private $db_charset='utf8';
+
     
     public function dbConnection(){
         
         try{
-            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
+            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name.';charset='.$this->db_charset,$this->db_username,$this->db_password,);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
