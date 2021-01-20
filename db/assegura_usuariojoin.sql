b3e3612e501e9ae5bcb87814289045364101ec53
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-20 13:01:51 -0300

    Att 01 join

--- db/assegura_usuariojoin.sql
+++ db/assegura_usuariojoin.sql
@@ -16,33 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `voluntarios`
+-- Table structure for table `usuariojoin`
 --
 
-DROP TABLE IF EXISTS `voluntarios`;
+DROP TABLE IF EXISTS `usuariojoin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `voluntarios` (
+CREATE TABLE `usuariojoin` (
   `id` int(11) NOT NULL,
   `nome` varchar(20) NOT NULL,
   `sobrenome` varchar(20) DEFAULT NULL,
+  `dataNascimento` varchar(20) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
-  `tipo` varchar(20) DEFAULT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `senha` varchar(250) NOT NULL,
+  `endereco` varchar(20) DEFAULT NULL,
+  `complemento` varchar(20) DEFAULT NULL,
   `cidade` varchar(20) DEFAULT NULL,
   `estado` varchar(20) DEFAULT NULL,
   `cep` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `imagem` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id`) REFERENCES `parceiros` (`id`),
+  CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`id`) REFERENCES `voluntarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `voluntarios`
+-- Dumping data for table `usuariojoin`
 --
 
-LOCK TABLES `voluntarios` WRITE;
-/*!40000 ALTER TABLE `voluntarios` DISABLE KEYS */;
-INSERT INTO `voluntarios` VALUES (4,'teste','tres','teste3@teste.com','voluntario','SÃ£o Paulo','SP','07130000'),(9,'teste','oito','teste8@teste.com','Voluntário','Guarulhos','São Paulo','07134-060');
-/*!40000 ALTER TABLE `voluntarios` ENABLE KEYS */;
+LOCK TABLES `usuariojoin` WRITE;
+/*!40000 ALTER TABLE `usuariojoin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuariojoin` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
